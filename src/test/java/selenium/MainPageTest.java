ae47fd41eeb56f701aef4a5c14da3af6c3721dd1
VesiZaharieva <vesela.zaharieva@abv.bg>
2024-04-03 17:13:57 +0300

    preliminary Dev.bg homework

--- src/test/java/selenium/MainPageTest.java
+++ src/test/java/selenium/MainPageTest.java
@@ -1,12 +1,8 @@
 package selenium;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.testng.annotations.Test;
-import static org.testng.AssertJUnit.assertEquals;
+
 
 public class MainPageTest extends MainDriver {
     MainPageHeader mainPageHeader;
